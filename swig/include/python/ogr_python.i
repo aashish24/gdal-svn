aashish24/gdal-svn

commit 654882e813556b9e06b691970e78fdb7c65b94c1

comment out auto fetching of datefields (backport of r16075 #2765
git-svn-id: https://svn.osgeo.org/gdal/branches/1.6/gdal@16855 f0d54148-0727-0410-94bb-9a71ac55c965

swig/include/python/ogr_python.i
@@ -222,8 +222,8 @@ layer[0:4] would return a list of the first four features."""
             return self.GetFieldAsInteger(fld_index)
         if fld_type == OFTReal:
             return self.GetFieldAsDouble(fld_index)
-        if fld_type == OFTDateTime or fld_type == OFTDate or fld_type == OFTTime:
-            return self.GetFieldAsDate(fld_index)
+#        if fld_type == OFTDateTime or fld_type == OFTDate or fld_type == OFTTime:
+#            return self.GetFieldAsDate(fld_index)
         # default to returning as a string.  Should we add more types?
         return self.GetFieldAsString(fld_index)
     

commit 16a0c1fbef38d2f034d23c6203f01a961e22e8d9

Fix for the problem introduced with r16855
git-svn-id: https://svn.osgeo.org/gdal/branches/1.6/gdal@16869 f0d54148-0727-0410-94bb-9a71ac55c965

swig/include/python/ogr_python.i
@@ -222,7 +222,7 @@ layer[0:4] would return a list of the first four features."""
             return self.GetFieldAsInteger(fld_index)
         if fld_type == OFTReal:
             return self.GetFieldAsDouble(fld_index)
-#        if fld_type == OFTDateTime or fld_type == OFTDate or fld_type == OFTTime:
+##        if fld_type == OFTDateTime or fld_type == OFTDate or fld_type == OFTTime:
 #            return self.GetFieldAsDate(fld_index)
         # default to returning as a string.  Should we add more types?
         return self.GetFieldAsString(fld_index)