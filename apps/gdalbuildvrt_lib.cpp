aashish24/gdal-svn

commit dbf1e8df0631474c0ff77fa3df82a7a7d0621d62

Allow GDALBuildVRTOptionsNew() to get the input_file_list from a vsi filesystem.
Related to #6355.

Also minor cleanup:
- Order includes
- C casts -> C++ casts


git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@33731 f0d54148-0727-0410-94bb-9a71ac55c965

apps/gdalbuildvrt_lib.cpp
@@ -2,7 +2,8 @@
  * $Id$
  *
  * Project:  GDAL Utilities
- * Purpose:  Command line application to build VRT datasets from raster products or content of SHP tile index
+ * Purpose:  Command line application to build VRT datasets from raster products
+ *           or content of SHP tile index
  * Author:   Even Rouault, <even dot rouault at spatialys dot com>
  *
  ******************************************************************************
@@ -27,10 +28,10 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
+#include "cpl_string.h"
+#include "gdal_proxy.h"
 #include "gdal_utils.h"
 #include "gdal_utils_priv.h"
-#include "gdal_proxy.h"
-#include "cpl_string.h"
 #include "gdal_vrt.h"
 #include "vrtdataset.h"
 
@@ -1315,7 +1316,8 @@ struct GDALBuildVRTOptions
 static
 GDALBuildVRTOptions* GDALBuildVRTOptionsClone(const GDALBuildVRTOptions *psOptionsIn)
 {
-    GDALBuildVRTOptions* psOptions = (GDALBuildVRTOptions*) CPLMalloc(sizeof(GDALBuildVRTOptions));
+    GDALBuildVRTOptions* psOptions = static_cast<GDALBuildVRTOptions*>(
+        CPLMalloc(sizeof(GDALBuildVRTOptions)) );
     memcpy(psOptions, psOptionsIn, sizeof(GDALBuildVRTOptions));
     if( psOptionsIn->pszResolution ) psOptions->pszResolution = CPLStrdup(psOptionsIn->pszResolution);
     if( psOptionsIn->pszSrcNoData ) psOptions->pszSrcNoData = CPLStrdup(psOptionsIn->pszSrcNoData);
@@ -1495,9 +1497,10 @@ static char *SanitizeSRS( const char *pszUserInput )
  */
 
 GDALBuildVRTOptions *GDALBuildVRTOptionsNew(char** papszArgv,
-                                          GDALBuildVRTOptionsForBinary* psOptionsForBinary)
+                                            GDALBuildVRTOptionsForBinary* psOptionsForBinary)
 {
-    GDALBuildVRTOptions *psOptions = (GDALBuildVRTOptions *)CPLCalloc(1, sizeof(GDALBuildVRTOptions));
+    GDALBuildVRTOptions *psOptions = static_cast<GDALBuildVRTOptions *>(
+        CPLCalloc(1, sizeof(GDALBuildVRTOptions)) );
 
     const char *tile_index = "location";
 
@@ -1537,19 +1540,19 @@ GDALBuildVRTOptions *GDALBuildVRTOptionsNew(char** papszArgv,
             if( psOptionsForBinary )
             {
                 const char* input_file_list = papszArgv[iArg];
-                FILE* f = VSIFOpen(input_file_list, "r");
+                VSILFILE* f = VSIFOpenL(input_file_list, "r");
                 if (f)
                 {
                     while(1)
                     {
-                        const char* filename = CPLReadLine(f);
+                        const char* filename = CPLReadLineL(f);
                         if (filename == NULL)
                             break;
                         if( !add_file_to_list(filename, tile_index,
                                          &psOptionsForBinary->nSrcFiles,
                                          &psOptionsForBinary->papszSrcFiles) )
                         {
-                            VSIFClose(f);
+                            VSIFCloseL(f);
                             GDALBuildVRTOptionsFree(psOptions);
                             return NULL;
                         }

commit 616ae62f71201c6beb0cc789772895d45b1a599c

Fix build broken by r33731
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@33732 f0d54148-0727-0410-94bb-9a71ac55c965

apps/gdalbuildvrt_lib.cpp
@@ -1557,7 +1557,7 @@ GDALBuildVRTOptions *GDALBuildVRTOptionsNew(char** papszArgv,
                             return NULL;
                         }
                     }
-                    VSIFClose(f);
+                    VSIFCloseL(f);
                 }
             }
             else