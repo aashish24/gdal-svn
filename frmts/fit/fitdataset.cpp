aashish24/gdal-svn

commit 582df8376b41eb4548407b17b2e833ef6eb9a55f

Tabs -> spaces.
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@34929 f0d54148-0727-0410-94bb-9a71ac55c965

frmts/fit/fitdataset.cpp
@@ -43,7 +43,7 @@ using namespace gstEndian;
 
 /************************************************************************/
 /* ==================================================================== */
-/*				FITDataset				*/
+/*                              FITDataset                              */
 /* ==================================================================== */
 /************************************************************************/
 
@@ -53,8 +53,8 @@ class FITDataset : public GDALPamDataset
 {
     friend class FITRasterBand;
 
-    VSILFILE	*fp;
-    FITinfo	*info;
+    VSILFILE    *fp;
+    FITinfo     *info;
     double      adfGeoTransform[6];
 
   public:
@@ -197,7 +197,7 @@ CPLErr FITRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
                                   void * pImage )
 
 {
-    FITDataset	*poFIT_DS = (FITDataset *) poDS;
+    FITDataset *poFIT_DS = (FITDataset *) poDS;
 
     uint64 tilenum = 0;
 
@@ -259,7 +259,7 @@ CPLErr FITRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
     if ( VSIFSeekL( poFIT_DS->fp, offset, SEEK_SET ) == -1 ) {
         CPLError(CE_Failure, CPLE_NotSupported,
                  "FIT - 64bit file seek failure, handle=%p", poFIT_DS->fp );
-	    return CE_Failure;
+            return CE_Failure;
     }
 
     // XXX - should handle status
@@ -488,7 +488,7 @@ CPLErr FITRasterBand::ReadBlock( int nBlockXOff, int nBlockYOff,
                                  void * pImage )
 
 {
-    FITDataset	*poFIT_DS = (FITDataset *) poDS;
+    FITDataset *poFIT_DS = (FITDataset *) poDS;
 
     return CE_None;
 }
@@ -501,7 +501,7 @@ CPLErr FITRasterBand::WriteBlock( int nBlockXOff, int nBlockYOff,
                                  void * pImage )
 
 {
-    FITDataset	*poFIT_DS = (FITDataset *) poDS;
+    FITDataset *poFIT_DS = (FITDataset *) poDS;
 
     return CE_None;
 }
@@ -556,7 +556,7 @@ double FITRasterBand::GetMaximum( int *pbSuccess )
 
 GDALColorInterp FITRasterBand::GetColorInterpretation()
 {
-    FITDataset	*poFIT_DS = (FITDataset *) poDS;
+    FITDataset *poFIT_DS = (FITDataset *) poDS;
 
     if ((! poFIT_DS) || (! poFIT_DS->info))
         return GCI_Undefined;
@@ -814,19 +814,19 @@ public:
     DeleteGuard( T *p ) : _ptr( p ) { }
     ~DeleteGuard()
     {
-	    delete _ptr;
+        delete _ptr;
     }
 
     T *take()
     {
         T *tmp = _ptr;
-	    _ptr = NULL;
-	    return tmp;
+        ptr = NULL;
+        return tmp;
     }
 
 private:
     T *_ptr;
-	// prevent default copy constructor and assignment operator
+    // prevent default copy constructor and assignment operator
     DeleteGuard( const DeleteGuard & );
     DeleteGuard &operator=( const DeleteGuard & );
 };
@@ -840,20 +840,20 @@ public:
     FreeGuard( T *p ) : _ptr( p ) { }
     ~FreeGuard()
     {
-	    if ( _ptr )
-			free( _ptr );
+        if ( _ptr )
+            free( _ptr );
     }
 
     T *take()
     {
         T *tmp = _ptr;
-	    _ptr = NULL;
-	    return tmp;
+        ptr = NULL;
+        return tmp;
     }
 
 private:
     T *_ptr;
-	// prevent default copy constructor and assignment operator
+    // prevent default copy constructor and assignment operator
     FreeGuard( const FreeGuard & );
     FreeGuard &operator=( const FreeGuard & );
 };
@@ -865,8 +865,8 @@ private:
 GDALDataset *FITDataset::Open( GDALOpenInfo * poOpenInfo )
 {
 /* -------------------------------------------------------------------- */
-/*	First we check to see if the file has the expected header	*/
-/*	bytes.								*/
+/*      First we check to see if the file has the expected header       */
+/*      bytes.                                                          */
 /* -------------------------------------------------------------------- */
 
     if( poOpenInfo->nHeaderBytes < 5 )
@@ -888,20 +888,21 @@ GDALDataset *FITDataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*      Create a corresponding GDALDataset.                             */
 /* -------------------------------------------------------------------- */
-    FITDataset 	*poDS = new FITDataset();
+    FITDataset *poDS = new FITDataset();
     DeleteGuard<FITDataset> guard( poDS );
 
-	// re-open file for large file (64bit) access
+    // Re-open file for large file (64bit) access.
     if ( poOpenInfo->eAccess == GA_ReadOnly )
-	poDS->fp = VSIFOpenL( poOpenInfo->pszFilename, "rb" );
+        poDS->fp = VSIFOpenL( poOpenInfo->pszFilename, "rb" );
     else
-	poDS->fp = VSIFOpenL( poOpenInfo->pszFilename, "r+b" );
+        poDS->fp = VSIFOpenL( poOpenInfo->pszFilename, "r+b" );
 
-    if ( !poDS->fp ) {
-	    CPLError( CE_Failure, CPLE_OpenFailed,
-		"Failed to re-open %s with FIT driver.\n",
-		poOpenInfo->pszFilename );
-	    return NULL;
+    if ( !poDS->fp )
+    {
+        CPLError( CE_Failure, CPLE_OpenFailed,
+                  "Failed to re-open %s with FIT driver.\n",
+                  poOpenInfo->pszFilename );
+        return NULL;
     }
     poDS->eAccess = poOpenInfo->eAccess;
 
@@ -923,11 +924,11 @@ GDALDataset *FITDataset::Open( GDALOpenInfo * poOpenInfo )
         CPLDebug("FIT", "Loading file with header version 02");
 
         gst_swapb(head->minValue);
-	info->minValue = head->minValue;
+        info->minValue = head->minValue;
         gst_swapb(head->maxValue);
-	info->maxValue = head->maxValue;
+        info->maxValue = head->maxValue;
         gst_swapb(head->dataOffset);
-	info->dataOffset = head->dataOffset;
+        info->dataOffset = head->dataOffset;
 
         info->userOffset = sizeof(FIThead02);
     }
@@ -939,9 +940,9 @@ GDALDataset *FITDataset::Open( GDALOpenInfo * poOpenInfo )
         CPLDebug("FIT", "Loading file with header version 01");
 
         // map old style header into new header structure
-	FIThead01* head01 = (FIThead01*)head;
+        FIThead01* head01 = (FIThead01*)head;
         gst_swapb(head->dataOffset);
-	info->dataOffset = head01->dataOffset;
+        info->dataOffset = head01->dataOffset;
 
         info->userOffset = sizeof(FIThead01);
     }

frmts/gxf/gxfdataset.cpp
@@ -35,7 +35,7 @@ CPL_CVSID("$Id$");
 
 /************************************************************************/
 /* ==================================================================== */
-/*				GXFDataset				*/
+/*                              GXFDataset                              */
 /* ==================================================================== */
 /************************************************************************/
 
@@ -45,19 +45,19 @@ class GXFDataset : public GDALPamDataset
 {
     friend class GXFRasterBand;
 
-    GXFHandle	hGXF;
+    GXFHandle   hGXF;
 
-    char	*pszProjection;
+    char        *pszProjection;
     double      dfNoDataValue;
     GDALDataType eDataType;
 
   public:
                 GXFDataset();
-		~GXFDataset();
+                ~GXFDataset();
 
     static GDALDataset *Open( GDALOpenInfo * );
 
-    CPLErr 	GetGeoTransform( double * padfTransform );
+    CPLErr      GetGeoTransform( double * padfTransform );
     const char *GetProjectionRef();
 };
 
@@ -73,7 +73,7 @@ class GXFRasterBand : public GDALPamRasterBand
 
   public:
 
-    		GXFRasterBand( GXFDataset *, int );
+                GXFRasterBand( GXFDataset *, int );
     double      GetNoDataValue(int* bGotNoDataValue);
 
     virtual CPLErr IReadBlock( int, int, void * );
@@ -103,7 +103,7 @@ GXFRasterBand::GXFRasterBand( GXFDataset *poDSIn, int nBandIn )
 double GXFRasterBand::GetNoDataValue(int* bGotNoDataValue)
 
 {
-    GXFDataset	*poGXF_DS = (GXFDataset *) poDS;
+    GXFDataset *poGXF_DS = (GXFDataset *) poDS;
     if (bGotNoDataValue)
         *bGotNoDataValue = (fabs(poGXF_DS->dfNoDataValue - -1e12) > .1);
     if (eDataType == GDT_Float32)
@@ -120,37 +120,37 @@ CPLErr GXFRasterBand::IReadBlock( CPL_UNUSED int nBlockXOff,
                                   int nBlockYOff,
                                   void * pImage )
 {
-    GXFDataset	*poGXF_DS = (GXFDataset *) poDS;
-    double	*padfBuffer;
-    float	*pafBuffer = (float *) pImage;
-    int		i;
-    CPLErr	eErr;
+    GXFDataset *poGXF_DS = (GXFDataset *) poDS;
+    double *padfBuffer;
+    float *pafBuffer = (float *) pImage;
+    int i;
+    CPLErr eErr;
 
-    CPLAssert( nBlockXOff == 0 );
+    CPLssert( nBlockXOff == 0 );
 
-    if (eDataType == GDT_Float32)
+    if eDataType == GDT_Float32)
     {
-        padfBuffer = (double *) VSIMalloc2(sizeof(double), nBlockXSize);
-        if( padfBuffer == NULL )
-            return CE_Failure;
-        eErr = GXFGetScanline( poGXF_DS->hGXF, nBlockYOff, padfBuffer );
+       padfBuffer = (double *) VSIMalloc2(sizeof(double), nBlockXSize);
+       if( padfBuffer == NULL )
+           return CE_Failure;
+       eErr = GXFGetScanline( poGXF_DS->hGXF, nBlockYOff, padfBuffer );
 
-        for( i = 0; i < nBlockXSize; i++ )
-            pafBuffer[i] = (float) padfBuffer[i];
+       for( i = 0; i < nBlockXSize; i++ )
+           pafBuffer[i] = (float) padfBuffer[i];
 
-        CPLFree( padfBuffer );
+       CPLFree( padfBuffer );
     }
-    else if (eDataType == GDT_Float64)
-        eErr = GXFGetScanline( poGXF_DS->hGXF, nBlockYOff, (double*)pImage );
-    else
-        eErr = CE_Failure;
+    els if (eDataType == GDT_Float64)
+       eErr = GXFGetScanline( poGXF_DS->hGXF, nBlockYOff, (double*)pImage );
+    els
+       eErr = CE_Failure;
 
-    return eErr;
+    retrn eErr;
 }
 
 /************************************************************************/
 /* ==================================================================== */
-/*				GXFDataset				*/
+/*                              GXFDataset                              */
 /* ==================================================================== */
 /************************************************************************/
 
@@ -188,8 +188,8 @@ GXFDataset::~GXFDataset()
 CPLErr GXFDataset::GetGeoTransform( double * padfTransform )
 
 {
-    CPLErr	eErr;
-    double	dfXOrigin, dfYOrigin, dfXSize, dfYSize, dfRotation;
+    CPLErr eErr;
+    double dfXOrigin, dfYOrigin, dfXSize, dfYSize, dfRotation;
 
     eErr = GXFGetPosition( hGXF, &dfXOrigin, &dfYOrigin,
                            &dfXSize, &dfYSize, &dfRotation );
@@ -229,8 +229,8 @@ const char *GXFDataset::GetProjectionRef()
 GDALDataset *GXFDataset::Open( GDALOpenInfo * poOpenInfo )
 
 {
-    GXFHandle	l_hGXF;
-    int		i, bFoundKeyword, bFoundIllegal;
+    GXFHandle l_hGXF;
+    int i, bFoundKeyword, bFoundIllegal;
 
 /* -------------------------------------------------------------------- */
 /*      Before trying GXFOpen() we first verify that there is at        */
@@ -318,9 +318,7 @@ GDALDataset *GXFDataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*      Create a corresponding GDALDataset.                             */
 /* -------------------------------------------------------------------- */
-    GXFDataset 	*poDS;
-
-    poDS = new GXFDataset();
+    GXFDataset *poDS = new GXFDataset();
 
     const char* pszGXFDataType = CPLGetConfigOption("GXF_DATATYPE", "Float32");
     GDALDataType eDT = GDALGetDataTypeByName(pszGXFDataType);
@@ -335,7 +333,7 @@ GDALDataset *GXFDataset::Open( GDALOpenInfo * poOpenInfo )
     poDS->eDataType = eDT;
 
 /* -------------------------------------------------------------------- */
-/*	Establish the projection.					*/
+/*      Establish the projection.                                       */
 /* -------------------------------------------------------------------- */
     poDS->pszProjection = GXFGetMapProjectionAsOGCWKT( l_hGXF );
 

frmts/cosar/cosar_dataset.cpp
@@ -39,7 +39,7 @@ const static int RS_OFFSET = 8;  /* Range Samples, the length of a range line */
 // #define AS_OFFSET    12 /* Azimuth Samples, the length of an azimuth column */
 // #define BI_OFFSET    16 /* Burst Index, the index number of the burst */
 const static int RTNB_OFFSET = 20;  /* Rangeline total number of bytes, incl. annot. */
-// #define TNL_OFFSET	24 /* Total Number of Lines */
+// #define TNL_OFFSET   24 /* Total Number of Lines */
 const static int MAGIC1_OFFSET = 28; /* Magic number 1: 0x43534152 */
 // #define MAGIC2_OFFSET 32 /* Magic number 2: Version number */
 
@@ -50,9 +50,9 @@ class COSARDataset : public GDALDataset
 {
 public:
         COSARDataset() : fp(NULL) { }
-	VSILFILE *fp;
+        VSILFILE *fp;
 
-	static GDALDataset *Open( GDALOpenInfo * );
+        static GDALDataset *Open( GDALOpenInfo * );
 };
 
 class COSARRasterBand : public GDALRasterBand
@@ -60,8 +60,8 @@ class COSARRasterBand : public GDALRasterBand
     unsigned long nRTNB;
     // int nBurstNumber;
 public:
-	COSARRasterBand(COSARDataset *, unsigned long nRTNB);
-	virtual CPLErr IReadBlock(int, int, void *);
+        COSARRasterBand(COSARDataset *, unsigned long nRTNB);
+        virtual CPLErr IReadBlock(int, int, void *);
 };
 
 /*****************************************************************************
@@ -71,10 +71,10 @@ public:
 COSARRasterBand::COSARRasterBand(COSARDataset *pDS, unsigned long nRTNBIn)  // :
     // nBurstNumber(1)
 {
-	this->nRTNB = nRTNBIn;
-	nBlockXSize = pDS->GetRasterXSize();
-	nBlockYSize = 1;
-	eDataType = GDT_CInt16;
+        nRTNB = nRTNBIn;
+        nBlockXSize = pDS->GetRasterXSize();
+        nBlockYSize = 1;
+        eDataType = GDT_CInt16;
 }
 
 CPLErr COSARRasterBand::IReadBlock(CPL_UNUSED int nBlockXOff,

frmts/dds/ddsdataset.cpp
@@ -48,7 +48,7 @@ enum { DDS_COLOR_TYPE_RGB,
 
 /************************************************************************/
 /* ==================================================================== */
-/*				DDSDataset				*/
+/*                              DDSDataset                              */
 /* ==================================================================== */
 /************************************************************************/
 
@@ -148,7 +148,7 @@ DDSDataset::CreateCopy(const char * pszFilename, GDALDataset *poSrcDS,
             fmt = cCRNFmtDXT3;
         else if (EQUAL(pszFormat, "dxt5"))
             fmt = cCRNFmtDXT5;
-	else if (EQUAL(pszFormat, "etc1"))
+        else if (EQUAL(pszFormat, "etc1"))
             fmt = cCRNFmtETC1;
         else
         {

frmts/dods/dodsdataset2.cpp
@@ -35,7 +35,7 @@
 
 #include <debug.h>
 
-#include <BaseType.h>		// DODS
+#include <BaseType.h>  // DODS
 #include <Byte.h>
 #include <Int16.h>
 #include <UInt16.h>
@@ -85,8 +85,8 @@ const char *nlat = "Northernmost_Latitude"; ///<
 const char *slat = "Southernmost_Latitude"; ///<
 const char *wlon = "Westernmost_Longitude"; ///<
 const char *elon = "Easternmost_Longitude"; ///<
-const char *gcs = "GeographicCS";	    ///<
-const char *pcs = "ProjectionCS";	    ///<
+const char *gcs = "GeographicCS";           ///<
+const char *pcs = "ProjectionCS";           ///<
 const char *norm_proj_param = "Norm_Proj_Param"; ///<
 const char *spatial_ref = "spatial_ref";    ///<
 //@}
@@ -116,13 +116,13 @@ get_variable(DDS &dds, const string &n)
 {
     BaseType *poBT = dds.var(www2id(n));
     if (!poBT) {
-	try {
-	    string leaf = n.substr(n.find_last_of('.')+1);
-	    poBT = dds.var(www2id(leaf));
-	}
-	catch (const std::exception &e) {
-	    poBT = 0;
-	}
+        try {
+            string leaf = n.substr(n.find_last_of('.')+1);
+            poBT = dds.var(www2id(leaf));
+        }
+        catch (const std::exception &e) {
+            poBT = 0;
+        }
     }
 
     return poBT;
@@ -207,12 +207,12 @@ static int GetDimension( string oCE, const char *pszDimName,
 class DODSDataset : public GDALDataset
 {
 private:
-    AISConnect *poConnect; 	//< Virtual connection to the data source
+    AISConnect *poConnect;      //< Virtual connection to the data source
 
-    string oURL;		//< data source URL
+    string oURL;                //< data source URL
     double adfGeoTransform[6];
     int    bGotGeoTransform;
-    string oWKT;		//< Constructed WKT string
+    string oWKT;                //< Constructed WKT string
 
     DAS    oDAS;
     DDS   *poDDS;
@@ -264,14 +264,14 @@ class DODSRasterBand : public GDALRasterBand
 {
 private:
     string oVarName;
-    string oCE;		        // Holds the CE (with [x] and [y] still there
+    string oCE;                 // Holds the CE (with [x] and [y] still there
 
     friend class DODSDataset;
 
     GDALColorInterp eColorInterp;
     GDALColorTable  *poCT;
 
-    int		   nOverviewCount;
+    int    nOverviewCount;
     DODSRasterBand **papoOverviewBand;
 
     int    nOverviewFactor;     // 1 for base, or 2/4/8 for overviews.
@@ -293,7 +293,7 @@ public:
     virtual CPLErr IReadBlock(int, int, void *);
     virtual GDALColorInterp GetColorInterpretation();
     virtual GDALColorTable *GetColorTable();
-		virtual CPLErr          SetNoDataValue( double );
+    virtual CPLErr          SetNoDataValue( double );
     virtual double          GetNoDataValue( int * );
 };
 
@@ -348,8 +348,8 @@ DODSDataset::connect_to_server() throw(Error)
 {
     // does the string start with 'http?'
     if (oURL.find("http://") == string::npos
-	&& oURL.find("https://") == string::npos)
-	throw Error(
+        && oURL.find("https://") == string::npos)
+        throw Error(
             "The URL does not start with 'http' or 'https,' I won't try connecting.");
 
 /* -------------------------------------------------------------------- */
@@ -706,16 +706,16 @@ void DODSDataset::HarvestDAS()
     {
         poFileInfo = oDAS.get_table( "NC_GLOBAL" );
 
-	if( poFileInfo == NULL )
-	{
-	    poFileInfo = oDAS.get_table( "HDF_GLOBAL" );
+        if( poFileInfo == NULL )
+        {
+            poFileInfo = oDAS.get_table( "HDF_GLOBAL" );
 
-	    if( poFileInfo == NULL )
-	    {
-	        CPLDebug( "DODS", "No GLOBAL DAS info." );
-	        return;
-	    }
-	}
+            if( poFileInfo == NULL )
+            {
+                CPLDebug( "DODS", "No GLOBAL DAS info." );
+                return;
+            }
+        }
     }
 #else
     AttrTable *poFileInfo = oDAS.find_container( "GLOBAL" );
@@ -724,16 +724,16 @@ void DODSDataset::HarvestDAS()
     {
         poFileInfo = oDAS.find_container( "NC_GLOBAL" );
 
-	if( poFileInfo == NULL )
-	{
-	    poFileInfo = oDAS.find_container( "HDF_GLOBAL" );
+        if( poFileInfo == NULL )
+        {
+            poFileInfo = oDAS.find_container( "HDF_GLOBAL" );
 
-	    if( poFileInfo == NULL )
-	    {
-	        CPLDebug( "DODS", "No GLOBAL DAS info." );
-	        return;
-	    }
-	}
+            if( poFileInfo == NULL )
+            {
+                CPLDebug( "DODS", "No GLOBAL DAS info." );
+                return;
+            }
+        }
     }
 #endif
 
@@ -1003,9 +1003,9 @@ DODSDataset::Open(GDALOpenInfo *poOpenInfo)
 /*      Get the AISConnect instance and the DAS and DDS for this        */
 /*      server.                                                         */
 /* -------------------------------------------------------------------- */
-	poDS->poConnect = poDS->connect_to_server();
-	poDS->poConnect->request_das(poDS->oDAS);
-	poDS->poConnect->request_dds(*(poDS->poDDS));
+        poDS->poConnect = poDS->connect_to_server();
+        poDS->poConnect->request_das(poDS->oDAS);
+        poDS->poConnect->request_dds(*(poDS->poDDS));
 
 /* -------------------------------------------------------------------- */
 /*      If we are given a constraint/projection list, then parse it     */
@@ -1037,7 +1037,7 @@ DODSDataset::Open(GDALOpenInfo *poOpenInfo)
         poDS->nRasterXSize = poBaseBand->GetXSize();
         poDS->nRasterYSize = poBaseBand->GetYSize();
 
-	poDS->SetBand(1, poBaseBand );
+        poDS->SetBand(1, poBaseBand );
 
         for( int iBand = 1; papszVarConstraintList[iBand*2] != NULL; iBand++ )
         {
@@ -1186,7 +1186,7 @@ DODSRasterBand::DODSRasterBand(DODSDataset *poDSIn, string oVarNameIn,
       case dods_float32_c: eDataType = GDT_Float32; break;
       case dods_float64_c: eDataType = GDT_Float64; break;
       default:
-	throw Error("The DODS GDAL driver supports only numeric data types.");
+        throw Error("The DODS GDAL driver supports only numeric data types.");
     }
 
 /* -------------------------------------------------------------------- */
@@ -1365,10 +1365,10 @@ void DODSRasterBand::HarvestDAS()
 /* -------------------------------------------------------------------- */
 /* Try _FillValue                                                       */
 /* -------------------------------------------------------------------- */
-	oValue = poBandInfo->get_attr( "_FillValue" );
-	if( oValue != "" ) {
-	    SetNoDataValue( CPLAtof(oValue.c_str()) );
-	}
+        oValue = poBandInfo->get_attr( "_FillValue" );
+        if( oValue != "" ) {
+            SetNoDataValue( CPLAtof(oValue.c_str()) );
+        }
     }
 
 
@@ -1493,9 +1493,10 @@ DODSRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 /* -------------------------------------------------------------------- */
         BaseType *poBt = get_variable(data, oVarName );
         if (!poBt)
-            throw Error(string("I could not read the variable '")
-		    + oVarName		    + string("' from the data source at:\n")
-		    + poDODS->GetUrl() );
+            throw Error(
+                string("I could not read the variable '")
+                + oVarName + string("' from the data source at:\n")
+                + poDODS->GetUrl() );
 
         Array *poA;
         switch (poBt->type()) {
@@ -1523,7 +1524,7 @@ DODSRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 /*      Dump the contents of the Array data into our output image buffer.*/
 /*                                                                      */
 /* -------------------------------------------------------------------- */
-        poA->buf2val(&pImage);	// !Suck the data out of the Array!
+        poA->buf2val(&pImage);  // !Suck the data out of the Array!
 
 /* -------------------------------------------------------------------- */
 /*      If the [x] dimension comes before [y], we need to transpose     */

frmts/dted/dteddataset.cpp
@@ -77,8 +77,8 @@ class DTEDRasterBand : public GDALPamRasterBand
 {
     friend class DTEDDataset;
 
-    int 	bNoDataSet;
-    double	dfNoDataValue;
+    int         bNoDataSet;
+    double      dfNoDataValue;
 
   public:
 

frmts/ecw/ecwcreatecopy.cpp
@@ -1002,7 +1002,7 @@ CPLErr GDALECWCompressor::Initialize(
         }
 
         m_OStream.Access( fpVSIL, TRUE, (BOOLEAN) bSeekable, pszFilename,
-			  0, -1 );
+                          0, -1 );
     }
     else
     {
@@ -1526,7 +1526,7 @@ ECWCreateCopyJPEG2000( const char * pszFilename, GDALDataset *poSrcDS,
 
 /************************************************************************/
 /* ==================================================================== */
-/*				ECWWriteDataset				*/
+/*                              ECWWriteDataset                         */
 /* ==================================================================== */
 /************************************************************************/
 

frmts/ecw/ecwdataset.cpp
@@ -2905,9 +2905,9 @@ char **ECWDataset::GetMetadata( const char *pszDomain )
 /************************************************************************/
 /*                   ReadFileMetaDataFromFile()                         */
 /*                                                                      */
-/* Gets relevant information from NCSFileMetadata and populates			*/
-/* GDAL metadata														*/
-/*																		*/
+/* Gets relevant information from NCSFileMetadata and populates         */
+/* GDAL metadata.                                                       */
+/*                                                                      */
 /************************************************************************/
 #if ECWSDK_VERSION >= 50
 void ECWDataset::ReadFileMetaDataFromFile()
@@ -3306,7 +3306,8 @@ void ECWInitialize()
     if( !CPLTestBool( CPLGetConfigOption("CONVERT_YCBCR_TO_RGB","YES") ) )
         NCSecwSetConfig(NCSCFG_JP2_MANAGE_ICC, FALSE);
 #if ECWSDK_VERSION>= 50
-	NCSecwSetConfig(NCSCFG_ECWP_CLIENT_HTTP_USER_AGENT, "ECW GDAL Driver/" NCS_ECWJP2_FULL_VERSION_STRING_DOT_DEL);
+    NCSecwSetConfig(NCSCFG_ECWP_CLIENT_HTTP_USER_AGENT,
+                    "ECW GDAL Driver/" NCS_ECWJP2_FULL_VERSION_STRING_DOT_DEL);
 #endif
 /* -------------------------------------------------------------------- */
 /*      Initialize cache memory limit.  Default is apparently 1/4 RAM.  */
@@ -3320,10 +3321,10 @@ void ECWInitialize()
         NCSecwSetConfig(NCSCFG_CACHE_MAXMEM, (UINT32) atoi(pszEcwCacheSize) );
 
     /* -------------------------------------------------------------------- */
-    /*      Version 3.x and 4.x of the ECWJP2 SDK did not resolve datum and         */
-    /*      projection to EPSG code using internal mapping.					*/
-    /*		Version 5.x do so we provide means to achieve old		*/
-    /*		behaviour.														*/
+    /*      Version 3.x and 4.x of the ECWJP2 SDK did not resolve datum and */
+    /*      projection to EPSG code using internal mapping.                 */
+    /*      Version 5.x do so we provide means to achieve old               */
+    /*      behaviour.                                                      */
     /* -------------------------------------------------------------------- */
     #if ECWSDK_VERSION >= 50
     if( CPLTestBool( CPLGetConfigOption("ECW_DO_NOT_RESOLVE_DATUM_PROJECTION","NO") ) == TRUE)

commit 4fd837292da687ae9142aaf75adab9920958eabd

Fix build failures in GXF and FIT drivers caused by r34929
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@34930 f0d54148-0727-0410-94bb-9a71ac55c965

frmts/fit/fitdataset.cpp
@@ -820,7 +820,7 @@ public:
     T *take()
     {
         T *tmp = _ptr;
-        ptr = NULL;
+        _ptr = NULL;
         return tmp;
     }
 
@@ -847,7 +847,7 @@ public:
     T *take()
     {
         T *tmp = _ptr;
-        ptr = NULL;
+        _ptr = NULL;
         return tmp;
     }
 

frmts/gxf/gxfdataset.cpp
@@ -126,9 +126,7 @@ CPLErr GXFRasterBand::IReadBlock( CPL_UNUSED int nBlockXOff,
     int i;
     CPLErr eErr;
 
-    CPLssert( nBlockXOff == 0 );
-
-    if eDataType == GDT_Float32)
+    if( eDataType == GDT_Float32)
     {
        padfBuffer = (double *) VSIMalloc2(sizeof(double), nBlockXSize);
        if( padfBuffer == NULL )
@@ -140,12 +138,12 @@ CPLErr GXFRasterBand::IReadBlock( CPL_UNUSED int nBlockXOff,
 
        CPLFree( padfBuffer );
     }
-    els if (eDataType == GDT_Float64)
+    else if (eDataType == GDT_Float64)
        eErr = GXFGetScanline( poGXF_DS->hGXF, nBlockYOff, (double*)pImage );
-    els
+    else
        eErr = CE_Failure;
 
-    retrn eErr;
+    return eErr;
 }
 
 /************************************************************************/