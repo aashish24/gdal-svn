aashish24/gdal-svn

commit 782426f15fc1f1375a090e4930eec464795ceed0

Combined pointer definition and initialization in ogrsf_frmts/ gpkg grass kml mitab nas ntf
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@35142 f0d54148-0727-0410-94bb-9a71ac55c965

ogr/ogrsf_frmts/ntf/ntfdump.cpp
@@ -81,17 +81,15 @@ int main( int argc, char ** argv )
 static void NTFCount( const char * pszFile )
 
 {
-    FILE      *fp;
-    NTFRecord *poRecord = NULL;
-    int       anCount[100], i;
-
-    for( i = 0; i < 100; i++ )
-        anCount[i] = 0;
-
-    fp = VSIFOpen( pszFile, "r" );
+    FILE *fp = VSIFOpen( pszFile, "r" );
     if( fp == NULL )
         return;
 
+    // int anCount[100] = {};
+    for( int i = 0; i < 100; i++ )
+        anCount[i] = 0;
+
+    NTFRecord *poRecord = NULL;
     do {
         if( poRecord != NULL )
             delete poRecord;

ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -1478,7 +1478,7 @@ CPLErr GDALGeoPackageDataset::IFlushCacheWithErrCode()
     if( m_bHasModifiedTiles )
     {
         const char* pszCurrentDate = CPLGetConfigOption("OGR_CURRENT_DATE", NULL);
-        char *pszSQL;
+        char *pszSQL = NULL ;
 
         if( pszCurrentDate )
         {
@@ -1875,7 +1875,7 @@ char **GDALGeoPackageDataset::GetMetadata( const char *pszDomain )
     if ( !HasMetadataTables() )
         return GDALPamDataset::GetMetadata( pszDomain );
 
-    char* pszSQL;
+    char* pszSQL = NULL;
     if( m_osRasterTable.size() )
     {
         pszSQL = sqlite3_mprintf(
@@ -2010,7 +2010,7 @@ void GDALGeoPackageDataset::WriteMetadata(CPLXMLNode* psXMLNode, /* will be dest
     }
     psXMLNode = NULL;
 
-    char* pszSQL;
+    char* pszSQL = NULL;
     if( pszTableName && pszTableName[0] != '\0' )
     {
         pszSQL = sqlite3_mprintf(
@@ -2331,7 +2331,7 @@ CPLErr GDALGeoPackageDataset::FlushMetadata()
         papszMDDup = CSLInsertString(papszMDDup, -1, *papszIter);
     }
 
-    CPLXMLNode* psXMLNode;
+    CPLXMLNode* psXMLNode = NULL;
     {
         GDALMultiDomainMetadata oLocalMDMD;
         char** papszDomainList = oMDMD.GetDomainList();
@@ -2478,7 +2478,6 @@ int GDALGeoPackageDataset::Create( const char * pszFilename,
                                    char **papszOptions )
 {
     CPLString osCommand;
-    const char *pszSpatialRefSysRecord;
 
     /* First, ensure there isn't any such file yet. */
     VSIStatBufL sStatBuf;
@@ -2565,7 +2564,7 @@ int GDALGeoPackageDataset::Create( const char * pszFilename,
         /* Requirement 11: The gpkg_spatial_ref_sys table in a GeoPackage SHALL */
         /* contain a record for EPSG:4326, the geodetic WGS84 SRS */
         /* http://opengis.github.io/geopackage/#spatial_ref_sys */
-        pszSpatialRefSysRecord =
+        const char *pszSpatialRefSysRecord =
             "INSERT INTO gpkg_spatial_ref_sys ("
             "srs_name, srs_id, organization, organization_coordsys_id, definition, description"
             ") VALUES ("
@@ -3521,8 +3520,6 @@ OGRLayer* GDALGeoPackageDataset::ICreateLayer( const char * pszLayerName,
 
 OGRErr GDALGeoPackageDataset::DeleteLayer( int iLayer )
 {
-    char *pszSQL;
-
     if( !bUpdate || iLayer < 0 || iLayer >= m_nLayers )
         return OGRERR_FAILURE;
 
@@ -3542,9 +3539,7 @@ OGRErr GDALGeoPackageDataset::DeleteLayer( int iLayer )
     if (osLayerName.size() == 0)
         return OGRERR_NONE;
 
-    pszSQL = sqlite3_mprintf(
-            "DROP TABLE \"%s\"",
-             osLayerName.c_str());
+    char *pszSQL = sqlite3_mprintf("DROP TABLE \"%s\"", osLayerName.c_str());
 
     SQLCommand(hDB, pszSQL);
     sqlite3_free(pszSQL);
@@ -3816,7 +3811,7 @@ void GDALGeoPackageDataset::CheckUnknownExtensions(bool bCheckRasterTable)
     if( !HasExtensionsTable() )
         return;
 
-    char* pszSQL;
+    char* pszSQL = NULL;
     if( !bCheckRasterTable)
         pszSQL = sqlite3_mprintf(
             "SELECT extension_name, definition, scope FROM gpkg_extensions WHERE (table_name IS NULL AND extension_name != 'gdal_aspatial')"
@@ -4277,7 +4272,7 @@ void GPKG_GDAL_GetMimeType(sqlite3_context* pContext,
     GDALDriver* poDriver = (GDALDriver*)GDALIdentifyDriver(osMemFileName, NULL);
     if( poDriver != NULL )
     {
-        const char* pszRes;
+        const char* pszRes = NULL;
         if( EQUAL(poDriver->GetDescription(), "PNG") )
             pszRes = "image/png";
         else if( EQUAL(poDriver->GetDescription(), "JPEG") )

ogr/ogrsf_frmts/gpkg/ogrgeopackagetablelayer.cpp
@@ -83,7 +83,7 @@ OGRErr OGRGeoPackageTableLayer::SaveTimestamp()
     if ( ! poDb ) return OGRERR_FAILURE;
 
     const char* pszCurrentDate = CPLGetConfigOption("OGR_CURRENT_DATE", NULL);
-    char *pszSQL;
+    char *pszSQL = NULL;
 
     if( pszCurrentDate )
     {
@@ -560,7 +560,6 @@ OGRErr OGRGeoPackageTableLayer::ReadTableDefinition(bool bIsSpatial, bool bIsGpk
 {
     OGRErr err;
     SQLResult oResultTable;
-    char* pszSQL;
     bool bReadExtent = false;
     sqlite3* poDb = m_poDS->GetDB();
     OGREnvelope oExtent;
@@ -572,15 +571,15 @@ OGRErr OGRGeoPackageTableLayer::ReadTableDefinition(bool bIsSpatial, bool bIsGpk
     if( bIsGpkgTable )
     {
         /* Check that the table name is registered in gpkg_contents */
-        pszSQL = sqlite3_mprintf(
-                    "SELECT table_name, data_type, identifier, "
-                    "description, min_x, min_y, max_x, max_y, srs_id "
-                    "FROM gpkg_contents "
-                    "WHERE table_name = '%q'"
+        char* pszSQL = sqlite3_mprintf(
+            "SELECT table_name, data_type, identifier, "
+            "description, min_x, min_y, max_x, max_y, srs_id "
+            "FROM gpkg_contents "
+            "WHERE table_name = '%q'"
 #ifdef WORKAROUND_SQLITE3_BUGS
-                    " OR 0"
+            " OR 0"
 #endif
-                    ,m_pszTableName);
+            , m_pszTableName);
 
         SQLResult oResultContents;
         err = SQLQuery(poDb, pszSQL, &oResultContents);
@@ -674,7 +673,7 @@ OGRErr OGRGeoPackageTableLayer::ReadTableDefinition(bool bIsSpatial, bool bIsGpk
     /*  #|name|type|notnull|default|pk */
     /*  0|id|integer|0||1 */
     /*  1|name|varchar|0||0 */
-    pszSQL = sqlite3_mprintf("pragma table_info('%q')", m_pszTableName);
+    char* pszSQL = sqlite3_mprintf("pragma table_info('%q')", m_pszTableName);
     err = SQLQuery(poDb, pszSQL, &oResultTable);
     sqlite3_free(pszSQL);
 
@@ -933,8 +932,8 @@ OGRGeoPackageTableLayer::~OGRGeoPackageTableLayer()
     {
         const char* pszT = m_pszTableName;
         const char* pszC =m_poFeatureDefn->GetGeomFieldDefn(0)->GetNameRef();
-        char* pszSQL;
-        pszSQL = sqlite3_mprintf("DROP TABLE \"rtree_%s_%s\"", pszT, pszC);
+        char* pszSQL =
+            sqlite3_mprintf("DROP TABLE \"rtree_%s_%s\"", pszT, pszC);
         SQLCommand(m_poDS->GetDB(), pszSQL);
         sqlite3_free(pszSQL);
     }
@@ -1090,12 +1089,11 @@ OGRErr OGRGeoPackageTableLayer::CreateGeomField( OGRGeomFieldDefn *poGeomFieldIn
 /* -------------------------------------------------------------------- */
     if( !m_bDeferredCreation )
     {
-        char *pszSQL;
-
-        pszSQL = sqlite3_mprintf("ALTER TABLE \"%s\" ADD COLUMN \"%s\" %s%s",
-                                 m_pszTableName, oGeomField.GetNameRef(),
-                                 m_poDS->GetGeometryTypeString(oGeomField.GetType()),
-                                 !oGeomField.IsNullable() ? " NOT NULL DEFAULT ''" : "");
+        char *pszSQL = sqlite3_mprintf(
+            "ALTER TABLE \"%s\" ADD COLUMN \"%s\" %s%s",
+            m_pszTableName, oGeomField.GetNameRef(),
+            m_poDS->GetGeometryTypeString(oGeomField.GetType()),
+            !oGeomField.IsNullable() ? " NOT NULL DEFAULT ''" : "");
 
         OGRErr err = SQLCommand(m_poDS->GetDB(), pszSQL);
         sqlite3_free(pszSQL);
@@ -1791,7 +1789,6 @@ void OGRGeoPackageTableLayer::CreateSpatialIndexIfNecessary()
 
 bool OGRGeoPackageTableLayer::CreateSpatialIndex()
 {
-    char* pszSQL;
     OGRErr err;
 
     if( m_bDeferredCreation && RunDeferredCreationIfNecessary() != OGRERR_NONE )
@@ -1823,11 +1820,12 @@ bool OGRGeoPackageTableLayer::CreateSpatialIndex()
     m_poDS->SoftStartTransaction();
 
     /* Register the table in gpkg_extensions */
-    pszSQL = sqlite3_mprintf(
-                 "INSERT INTO gpkg_extensions "
-                 "(table_name,column_name,extension_name,definition,scope) "
-                 "VALUES ('%q', '%q', 'gpkg_rtree_index', 'GeoPackage 1.0 Specification Annex L', 'write-only')",
-                 pszT, pszC );
+    char* pszSQL = sqlite3_mprintf(
+        "INSERT INTO gpkg_extensions "
+        "(table_name,column_name,extension_name,definition,scope) "
+        "VALUES ('%q', '%q', 'gpkg_rtree_index', "
+        "'GeoPackage 1.0 Specification Annex L', 'write-only')",
+        pszT, pszC );
     err = SQLCommand(m_poDS->GetDB(), pszSQL);
     sqlite3_free(pszSQL);
     if( err != OGRERR_NONE )
@@ -2039,7 +2037,7 @@ void OGRGeoPackageTableLayer::CheckUnknownExtensions()
 
     /* We have only the SQL functions needed by the 3 following extensions */
     /* anything else will likely cause troubles */
-    char* pszSQL;
+    char* pszSQL = NULL;
 
     if( m_poFeatureDefn->GetGeomFieldCount() == 0 )
     {
@@ -2202,11 +2200,10 @@ bool OGRGeoPackageTableLayer::DropSpatialIndex(bool bCalledFromSQLFunction)
 
     const char* pszT = m_pszTableName;
     const char* pszC =m_poFeatureDefn->GetGeomFieldDefn(0)->GetNameRef();
-    char* pszSQL;
-
-    pszSQL = sqlite3_mprintf("DELETE FROM gpkg_extensions WHERE table_name='%q' "
-                 "AND column_name='%q' AND extension_name='gpkg_rtree_index'",
-                 pszT, pszC );
+    char* pszSQL = sqlite3_mprintf(
+        "DELETE FROM gpkg_extensions WHERE table_name='%q' "
+        "AND column_name='%q' AND extension_name='gpkg_rtree_index'",
+        pszT, pszC );
     SQLCommand(m_poDS->GetDB(), pszSQL);
     sqlite3_free(pszSQL);
 
@@ -2267,10 +2264,9 @@ void OGRGeoPackageTableLayer::RenameTo(const char* pszDstTableName)
     }
 
     /* We also need to update GeoPackage metadata tables */
-    char* pszSQL;
-    pszSQL = sqlite3_mprintf(
-            "UPDATE gpkg_geometry_columns SET table_name = '%s' WHERE table_name = '%s'",
-            pszDstTableName, m_pszTableName);
+    char* pszSQL = sqlite3_mprintf(
+        "UPDATE gpkg_geometry_columns SET table_name = '%s' WHERE table_name = '%s'",
+        pszDstTableName, m_pszTableName);
     SQLCommand(m_poDS->GetDB(), pszSQL);
     sqlite3_free(pszSQL);
 
@@ -2629,14 +2625,12 @@ char **OGRGeoPackageTableLayer::GetMetadata( const char *pszDomain )
     if ( !m_poDS->HasMetadataTables() )
         return OGRLayer::GetMetadata( pszDomain );
 
-    char* pszSQL;
-
-    pszSQL = sqlite3_mprintf(
-        "SELECT md.metadata, md.md_standard_uri, md.mime_type, mdr.reference_scope FROM gpkg_metadata md "
+    char* pszSQL = sqlite3_mprintf(
+        "SELECT md.metadata, md.md_standard_uri, md.mime_type, "
+        "mdr.reference_scope FROM gpkg_metadata md "
         "JOIN gpkg_metadata_reference mdr ON (md.id = mdr.md_file_id ) "
         "WHERE mdr.table_name = '%q' ORDER BY md.id",
         m_pszTableName);
-
 
     SQLResult oResult;
     OGRErr err = SQLQuery(m_poDS->GetDB(), pszSQL, &oResult);

ogr/ogrsf_frmts/gpkg/ogrgeopackageutility.cpp
@@ -156,12 +156,10 @@ GIntBig SQLGetInteger64(sqlite3 * poDb, const char * pszSQL, OGRErr *err)
 {
     CPLAssert( poDb != NULL );
 
-    sqlite3_stmt *poStmt;
-    int rc;
-    GIntBig i;
+    sqlite3_stmt *poStmt = NULL;
 
     /* Prepare the SQL */
-    rc = sqlite3_prepare_v2(poDb, pszSQL, -1, &poStmt, NULL);
+    int rc = sqlite3_prepare_v2(poDb, pszSQL, -1, &poStmt, NULL);
     if ( rc != SQLITE_OK )
     {
         CPLError( CE_Failure, CPLE_AppDefined, "sqlite3_prepare_v2(%s) failed: %s",
@@ -180,7 +178,7 @@ GIntBig SQLGetInteger64(sqlite3 * poDb, const char * pszSQL, OGRErr *err)
     }
 
     /* Read the integer from the row */
-    i = sqlite3_column_int64(poStmt, 0);
+    GIntBig i = sqlite3_column_int64(poStmt, 0);
     sqlite3_finalize(poStmt);
 
     if ( err ) *err = OGRERR_NONE;
@@ -391,7 +389,6 @@ GByte* GPkgGeometryFromOGR(const OGRGeometry *poGeometry, int iSrsId, size_t *ps
 {
     CPLAssert( poGeometry != NULL );
 
-    GByte *pabyPtr;
     GByte byFlags = 0;
     GByte byEnv = 1;
     OGRwkbByteOrder eByteOrder = (OGRwkbByteOrder)CPL_IS_LSB;
@@ -484,7 +481,7 @@ GByte* GPkgGeometryFromOGR(const OGRGeometry *poGeometry, int iSrsId, size_t *ps
         }
     }
 
-    pabyPtr = pabyWkb + szHeader;
+    GByte *pabyPtr = pabyWkb + szHeader;
 
     /* Use the wkbVariantIso for ISO SQL/MM output (differs for 3d geometry) */
     err = poGeometry->exportToWkb(eByteOrder, pabyPtr, wkbVariantIso);
@@ -621,7 +618,6 @@ OGRGeometry* GPkgGeometryToOGR(const GByte *pabyGpkg, size_t szGpkg, OGRSpatialR
     CPLAssert( pabyGpkg != NULL );
 
     GPkgHeader oHeader;
-    OGRGeometry *poGeom;
 
     /* Read header */
     OGRErr err = GPkgHeaderFromWKB(pabyGpkg, szGpkg, &oHeader);
@@ -633,6 +629,7 @@ OGRGeometry* GPkgGeometryToOGR(const GByte *pabyGpkg, size_t szGpkg, OGRSpatialR
     size_t szWkb = szGpkg - oHeader.szHeader;
 
     /* Parse WKB */
+    OGRGeometry *poGeom = NULL;
     err = OGRGeometryFactory::createFromWkb((GByte*)pabyWkb, poSrs, &poGeom,
                                             static_cast<int>(szWkb));
     if ( err != OGRERR_NONE )

ogr/ogrsf_frmts/grass/ogrgrassdatasource.cpp
@@ -214,9 +214,7 @@ int OGRGRASSDataSource::Open( const char * pszNewName, int bUpdate,
 
     for ( int i = 0; i < ncidx; i++ ) {
 	// Create the layer object
-	OGRGRASSLayer       *poLayer;
-
-        poLayer = new OGRGRASSLayer( i, &map );
+	OGRGRASSLayer *poLayer = new OGRGRASSLayer( i, &map );
 
         // Add layer to data source layer list
 	papoLayers = (OGRGRASSLayer **)

ogr/ogrsf_frmts/grass/ogrgrasslayer.cpp
@@ -133,7 +133,7 @@ OGRGRASSLayer::OGRGRASSLayer( int layerIndex,  struct Map_info * map )
 	if ( StartDbDriver() )
 	{
 	    db_set_string ( poDbString, poLink->table );
-	    dbTable *table;
+	    dbTable *table = NULL;
 	    if ( db_describe_table ( poDriver, poDbString, &table) == DB_OK )
 	    {
 		nFields = db_get_table_number_of_columns ( table );
@@ -904,11 +904,9 @@ OGRGeometry *OGRGRASSLayer::GetFeatureGeometry ( long nFeatureId, int *cat )
         {
 	    Vect_get_area_points ( poMap, id, poPoints );
 
-	    OGRPolygon 		*poOGRPoly;
-	    poOGRPoly = new OGRPolygon();
+	    OGRPolygon *poOGRPoly = new OGRPolygon();
 
-	    OGRLinearRing       *poRing;
-	    poRing = new OGRLinearRing();
+	    OGRLinearRing *poRing = new OGRLinearRing();
             if (bIs3D)
                 poRing->setPoints( poPoints->n_points,
                                 poPoints->x, poPoints->y, poPoints->z );

ogr/ogrsf_frmts/kml/ogrkmllayer.cpp
@@ -542,7 +542,7 @@ OGRErr OGRKMLLayer::ICreateFeature( OGRFeature* poFeature )
     {
         char* pszGeometry = NULL;
         OGREnvelope sGeomBounds;
-        OGRGeometry* poWGS84Geom;
+        OGRGeometry *poWGS84Geom = NULL;
 
         if (NULL != poCT_)
         {

ogr/ogrsf_frmts/mitab/mitab_middatafile.cpp
@@ -291,11 +291,7 @@ double MIDDATAFile::GetYTrans(double dfY)
 
 GBool MIDDATAFile::IsValidFeature(const char *pszString)
 {
-    char **papszToken ;
-
-    papszToken = CSLTokenizeString(pszString);
-
-    //   printf("%s\n",pszString);
+    char **papszToken = CSLTokenizeString(pszString);
 
     if (CSLCount(papszToken) == 0)
     {

commit 3149124cfacb7569a82b5659cea6db85b7a67abb

Fix ntfdump.cpp broken in r35142.
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@35143 f0d54148-0727-0410-94bb-9a71ac55c965

ogr/ogrsf_frmts/ntf/ntfdump.cpp
@@ -85,9 +85,7 @@ static void NTFCount( const char * pszFile )
     if( fp == NULL )
         return;
 
-    // int anCount[100] = {};
-    for( int i = 0; i < 100; i++ )
-        anCount[i] = 0;
+    int anCount[100] = {};
 
     NTFRecord *poRecord = NULL;
     do {
@@ -102,7 +100,7 @@ static void NTFCount( const char * pszFile )
     VSIFClose( fp );
 
     printf( "\nReporting on: %s\n", pszFile );
-    for( i = 0; i < 100; i++ )
+    for( int i = 0; i < 100; i++ )
     {
         if( anCount[i] > 0 )
             printf( "Found %d records of type %d\n", anCount[i], i );
@@ -116,14 +114,14 @@ static void NTFCount( const char * pszFile )
 static void NTFDump( const char * pszFile, char **papszOptions )
 
 {
-    OGRFeature         *poFeature;
-    OGRNTFDataSource   oDS;
+    OGRNTFDataSource oDS;
 
     oDS.SetOptionList( papszOptions );
 
     if( !oDS.Open( pszFile ) )
         return;
 
+    OGRFeature *poFeature = NULL;
     while( (poFeature = oDS.GetNextFeature()) != NULL )
     {
         printf( "-------------------------------------\n" );